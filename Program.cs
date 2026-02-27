23465f859a5a68c7d21323b4c9b8e990cd315f75
Bubnov <nekit.b.nb@mail.ru>
2026-02-27 04:30:45 +0300

    Добавлен файл Program.cs с выводом группы и ФИО

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Группа: ИС-22Б");
+        Console.WriteLine("ФИО: Бубнов Никита Валерьевич");
+    }
+}
